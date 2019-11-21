68c6752f56dd53fe801011560d48dda0daba4055
Matthew Rapati <mattrapati@gmail.com>
2019-11-20 22:40:29 -0500

    Added Stackoverflow flair to the "About" page.

--- src/pages/about.js
+++ src/pages/about.js
@@ -5,6 +5,7 @@ import Layout from "../components/layout"
 import SEO from "../components/seo"
 import ArticleHeader from "../components/article-header"
 import Bio from "../components/bio"
+import { rhythm } from "../utils/typography"
 
 const About = () => {
   const data = useStaticQuery(graphql`
@@ -50,6 +51,22 @@ const About = () => {
           If you're wondering why the name "Hamburger Sandwich", it's because
           one day many years ago I randomly checked if this domain was
           available, and it was. So I bought it and now it's my blog domain!
+        </p>
+        <p
+          style={{
+            marginTop: rhythm(2),
+            textAlign: `center`
+          }}
+        >
+          <a href="https://stackoverflow.com/users/15000/matthew-rapati">
+            <img
+              src="https://stackoverflow.com/users/flair/15000.png"
+              width="208"
+              height="58"
+              alt="profile for Matthew Rapati at Stack Overflow, Q&amp;A for professional and enthusiast programmers"
+              title="profile for Matthew Rapati at Stack Overflow, Q&amp;A for professional and enthusiast programmers"
+            />
+          </a>
         </p>
         <footer>
           <Bio />
